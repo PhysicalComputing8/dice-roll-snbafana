4fb247acdb043fd83a40d28fa1b93c7656e3549e
Trevor Shaw <shawt@genlrn.com>
2020-09-21 21:34:11 -0400

    Teacher Feedback

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+// Great job. You moved your commands inside of the onShake event trigger. Now they will execute when the micro:ibit is shaken.
 input.onGesture(Gesture.Shake, function () {
+    // uh oh! Unless your die has ten sides (plus some other dimensional side for a zero) I don't think this number range is correct. :-)
     roll = randint(0, 10)
     basic.showNumber(roll)
 })
