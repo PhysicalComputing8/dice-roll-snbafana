ed77f4f69fafd0f9b9697eaf48a2a90473d48517
Trevor Shaw <shawt@genlrn.com>
2020-09-23 11:31:26 -0400

    Teacher Comments - You can begin working on your game!

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let roll = 0
 // Great job. You moved your commands inside of the onShake event trigger. Now they will execute when the micro:ibit is shaken.
 input.onGesture(Gesture.Shake, function () {
+    // I like this animation effect. Great use of a loop to generate it!
     for (let index = 0; index <= 5; index++) {
         basic.showLeds(`
             . . . . .
@@ -14,6 +15,7 @@ input.onGesture(Gesture.Shake, function () {
     }
     // uh oh! Unless your die has ten sides (plus some other dimensional side for a zero) I don't think this number range is correct. :-)
     roll = randint(1, 6)
+    // Good use of if/then/else to display the appropriate image.
     if (roll == 1) {
         basic.showLeds(`
             . . . . .
